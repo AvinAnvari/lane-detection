67f64580bed6022974522ce99be49472459f1f00
Avin Anvari <avinanvari@gmail.com>
2026-08-30 16:51:43 -0700

    Add frame-by-frame video playback with quit key

--- lane_detection.py
+++ lane_detection.py
@@ -5,7 +5,18 @@ cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
     print("Error: Could not open video.")
-else:
-    print("Video opened succesfully.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+    cv2.imshow("Dashcam Feed",frame)
+    key = cv2.waitKey(25)
+    if key == ord('q'):
+        break
+
 
 cap.release()
+cv2.destroyAllWindows()

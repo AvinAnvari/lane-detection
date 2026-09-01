16f3677fad5ec0b7a58146b41af4be0847035e16
Avin Anvari <avinanvari@gmail.com>
2026-08-31 19:27:42 -0700

    Add hough line Transform for lane line detection

--- lane_detection.py
+++ lane_detection.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 video_path = "data/dashcam.mp4"
 cap = cv2.VideoCapture(video_path)
@@ -9,15 +10,31 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
-
     if not ret:
         break
+    frame = cv2.resize(frame, (960, 540))
+    
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(grayscale, (7,7), 0)
-    edge = cv2.Canny(blur, 80, 160)
-    cv2.imshow("Dashcam Feed",edge)
-    key = cv2.waitKey(25)
+    edge = cv2.Canny(blur, 60, 160)
+    points = np.array([[
+    (150, 300),
+    (785, 300),
+    (550, 150),
+    (400, 150)]])
+    mask = np.zeros(edge.shape, dtype=np.uint8)
+    filled = cv2.fillPoly(mask, points,255)
+    bitwised = cv2.bitwise_and(filled, edge)
+    # test_lines = cv2.polylines(frame,points, True, (0,0,255), 5)
+    lines = cv2.HoughLinesP(bitwised, 0.5, np.pi/180, 1, 2, 1)
 
+    if lines is not None:
+        for line in lines:
+                x1, y1, x2, y2 = line
+                cv2.line(frame, (x1,y1), (x2,y2), (0,255,0), 10)
+
+    cv2.imshow("Dashcam Feed",frame)
+    key = cv2.waitKey(25)
     if key == ord('q'):
         break
 
